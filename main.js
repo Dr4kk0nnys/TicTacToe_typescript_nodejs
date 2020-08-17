916c1f112b5712b6144a11ec40632173722592dd
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2020-08-17 19:22:59 -0300

    Populate Empty Board function added / Populate Fake board function for developing purposes only added / Read Game board added

--- main.js
+++ main.js
@@ -2,12 +2,30 @@ import PromptSync from 'prompt-sync';
 const input = PromptSync({ sigint: true });
 class Main {
     constructor() {
-        console.log('Hello World');
+        this.board = new Array(9);
+        this.populateFakeBoard();
+        this.readGameBoard();
+    }
+    populateEmptyBoard() {
+        this.board.fill(' ');
+    }
+    // testing / developing only
+    populateFakeBoard() {
+        const randomPossibilities = ['X', 'O'];
+        for (let i = 0; i < this.board.length; i++) {
+            this.board[i] = randomPossibilities[Math.floor(Math.random() * randomPossibilities.length)];
+        }
+    }
+    readGameBoard() {
+        for (let i = 0; i < 3; i++) {
+            let line = '';
+            for (let j = 0; j < 3; j++) {
+                line += this.board[i + j] + ' | ';
+            }
+            console.log(line);
+        }
     }
     handleUserInput() {
-        const userInput = input('Hi! u good ? ');
-        console.log(`It is a pleasure to meet you ${userInput}`);
     }
 }
 const main = new Main();
-main.handleUserInput();
--- main.ts
+++ main.ts
@@ -3,15 +3,40 @@ const input = PromptSync({ sigint: true });
 
 
 class Main {
+    board = new Array<string>(9);
+
     constructor() {
-        console.log('Hello World');
+        this.populateFakeBoard();
+        this.readGameBoard();
+    }
+
+    populateEmptyBoard(): void {
+        this.board.fill(' ');
+    }
+
+    // testing / developing only
+    populateFakeBoard(): void {
+        const randomPossibilities = ['X', 'O'];
+        for (let i = 0; i < this.board.length; i++) {
+            const randomNumber = Math.floor(Math.random() * randomPossibilities.length);
+            this.board[i] = randomPossibilities[randomNumber];
+        }
+    }
+
+    readGameBoard(): void {
+        for (let i = 0; i < 3; i++) {
+            let line = '';
+
+            for (let j = 0; j < 3; j++) {
+                line += this.board[i + j] + ' | ';
+            }
+
+            console.log(line);
+        }
     }
 
     handleUserInput(): void {
-        const userInput: string = input('Hi! u good ? ');
-        console.log(`It is a pleasure to meet you ${userInput}`);
     }
 }
 
 const main = new Main();
-main.handleUserInput();
